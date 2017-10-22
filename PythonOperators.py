f6a31bfc855b2e5bf8fbb34571c07ae73e2175ea
Wang Chuncheng <TTChengWang@foxmail.com>
2017-10-22 23:08:39 +0800

    add PythonOperators.py

---
+++ PythonOperators.py
@@ -0,0 +1,78 @@
+# python中的运算符
+# +
+print('oprand + :')
+print('2 + 3 = {0}'.format( 2 + 3))
+print('"id" + "ea" = {0}\n'.format("id"+"ea"))
+
+# -
+print('oprand - :')
+print('"- 8" = %d' % (- 8))
+print('5 - 4 = %d\n' %(5-4))
+
+# *
+print('oprand * :')
+print("2 * 3 = %d" %(2*3))
+print('"idea"*2 = {0}\n'.format("idea"*2))
+
+# /
+print('oprand / :')
+print("7 / 3 = {0}".format(7/3))
+print("7.0 / 3 = {0}\n".format(7.0/3))
+
+# // 商的整数部分
+print('oprand // :')
+print('7 // 3 = {0}\n'.format(7//3))
+
+# % 商的余数部分
+print('oprand % :')
+print('7 % 3 = {0}\n'.format(7%3))
+
+# ** 幂运算
+print('oprand ** :')
+print('2**3 = {0}\n'.format(2**3))
+
+# <  <=  >  >=
+print('oprand <  <=  >  >= :')
+print('2<3 = {0}'.format(2<3))
+print('3<2 = {0}\n'.format(3<2))
+
+# == !=
+print('oprand == !=:')
+print('2 == 2 {0}'.format(2 == 2 ))
+print('2 != 2 {0}\n'.format(2 != 2 ))
+
+# &
+print('oprand & :')
+print('7 & 18 = {0}\n'.format(7&18))
+
+# |
+print('oprand | :')
+print('7 | 18 = {0}\n'.format(7|18))
+
+# ^
+print('oprand ^ :')
+print('7 ^ 18 = {0}\n'.format(7^18))
+
+# ~  按位翻转 ~x = -(x+1)
+print('oprand ~ :')
+print('~ 18 = {0}\n'.format(~18))
+
+# >> 按位右移
+print('oprand >> :')
+print('18 >> 1 = {0}\n'.format(18 >> 1))
+
+# << 按位左移
+print('oprand << :')
+print('18 << 1 = {0}\n'.format(18 << 1))
+
+# not 逻辑非
+print('oprand not :')
+print('not True = {0}\n'.format(not True))
+
+# and 逻辑与
+print('oprand and :')
+print('false and True = {0}\n'.format(False and True))
+
+# or 逻辑或
+print('oprand or :')
+print('false or True = {0}\n'.format(False or True))
